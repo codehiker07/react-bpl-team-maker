aed9e5afc5e7be207216bd40a37b92cf50dd0078
codehiker07 <codehiker07@gmail.com>
2023-09-13 09:02:08 +0600

    button function added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,51 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import './Home.css';
 
 
 const Home = () => {
 
+    const [allActors, setAllActors] = useState([]);
+
+
     useEffect(() => {
         fetch('./data.json')
             .then(res => res.json())
-            .then(data =>console.log(data))
-    }, [])
+            .then(data =>setAllActors(data))
+    }, []);
+
+    const handleSelectActor = () => {
+        console.log('Hello');
+    }
+
+    console.log(allActors);
 
     return (
         <div className='container'>
-            <div className="card-container">
-                <div className="card">
-                    <div className="card-img">
-                        <img src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" className="photo" />
+            <div className="home-container">
+                <div className="card-container">
+                    { allActors.map((actor) => (
+                        <div key={actor.id} className="card">
+                        <div className="card-img">
+                            <img src={actor.image} alt="" className="photo" />
+                        </div>
+                        <h2>{actor.name}</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, facilis.</p>
+                        <div className="info">
+                            <p>Salary: {actor.salary}$</p>
+                            <p>{actor.role}</p>
+                        </div>
+                        <button onClick={handleSelectActor} className='card-btn'>Select</button>
                     </div>
-                    <h2>Motiur Rahman</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, facilis.</p>
-                    <div className="info">
-                        <p>Salary: 200$</p>
-                        <p>Writer</p>
-                    </div>
-                    <button className='card-btn'>Select</button>
+                    ))
+                    }
                 </div>
+                <div className="cart">
+                    <h2>This is cart</h2>
+                </div>
+
             </div>
         </div>
     );
